c0f95d9a03dfe174d5f3f517c3b0a3501b903d3a
paulolima1212 <plima12121984@gmail.com>
2022-12-06 18:06:55 +0000

    style: finished styles history

--- src/Pages/History/history.styles.ts
+++ src/Pages/History/history.styles.ts
@@ -1,44 +1,83 @@
 import styled from 'styled-components';
+import { TypeOf } from 'zod';
 
 export const HistoryContainer = styled.div`
   display: flex;
   flex: 1;
   color: ${({ theme }) => theme['gray-100']};
   flex-direction: column;
-
-  h1 {
-    padding: 1rem;
-  }
+  padding: 3.5rem;
 `;
 
 export const TableContainer = styled.div`
-  width: 100%
-  height: 100%
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 1rem;
+  flex: 1;
+  overflow: auto;
+  margin-top: 2rem;
 
   table {
-    height: 100%;
     width: 100%;
-    margin: 0 auto;
-    padding: 1rem;
+    border-collapse: collapse;
+    min-width: 600px;
 
-    thead {
+    th {
       background: ${({ theme }) => theme['gray-600']};
-      th {
-        padding: 0.5rem;
-        text-align: initial;
+      padding: 1rem;
+      text-align: left;
+      color: ${({ theme }) => theme['gray-100']};
+      font-size: 0.875rem;
+      line-height: 1.6;
+
+      &:first-child {
+        width: 50%;
+        border-top-left-radius: 8px;
+        padding-left: 1.5rem;
+      }
+
+      &:last-child {
+        border-top-right-radius: 8px;
+        padding-right: 1.5rem;
       }
     }
 
-    tbody {
-      border-collapse: collapse;
+    td {
+      background: ${({ theme }) => theme['gray-700']};
+      border-top: 4px solid ${({ theme }) => theme['gray-800']};
+      padding: 1rem;
+      font-size: 0.875rem;
+      line-height: 1.6;
 
-      td {
-        padding: 0.3rem;
+      &:first-child {
+        width: 50%;
+        padding-left: 1.5rem;
+      }
+
+      &:last-child {
+        padding-right: 1.5rem;
       }
     }
+  }
+`;
+
+const STATUS_COLOR = {
+  done: 'green-300',
+  inprogress: 'yellow-500',
+  interrupted: 'red-500',
+} as const;
+
+interface StatusTaskProps {
+  statusColor: keyof typeof STATUS_COLOR;
+}
+
+export const StatusTask = styled.span<StatusTaskProps>`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  &::before {
+    content: '';
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 50%;
+    background: ${({ theme, statusColor }) => theme[STATUS_COLOR[statusColor]]};
   }
 `;
--- src/Pages/History/index.tsx
+++ src/Pages/History/index.tsx
@@ -1,9 +1,9 @@
-import { HistoryContainer, TableContainer } from './history.styles';
+import { HistoryContainer, StatusTask, TableContainer } from './history.styles';
 
 export function History() {
   return (
     <HistoryContainer>
-      <h1>History</h1>
+      <h1>My History</h1>
       <TableContainer>
         <table>
           <thead>
@@ -19,31 +19,41 @@ export function History() {
               <td>Made a principal page of application</td>
               <td>25 minutes</td>
               <td>4h About</td>
-              <td>Done</td>
+              <td>
+                <StatusTask statusColor={'inprogress'}>In progress</StatusTask>
+              </td>
             </tr>
             <tr>
               <td>Made a principal page of application</td>
               <td>25 minutes</td>
               <td>4h About</td>
-              <td>Done</td>
+              <td>
+                <StatusTask statusColor={'done'}>Done</StatusTask>
+              </td>
             </tr>
             <tr>
               <td>Made a principal page of application</td>
               <td>25 minutes</td>
               <td>4h About</td>
-              <td>Done</td>
+              <td>
+                <StatusTask statusColor={'done'}>Done</StatusTask>
+              </td>
             </tr>
             <tr>
               <td>Made a principal page of application</td>
               <td>25 minutes</td>
               <td>4h About</td>
-              <td>Done</td>
+              <td>
+                <StatusTask statusColor={'interrupted'}>Interrupted</StatusTask>
+              </td>
             </tr>
             <tr>
               <td>Made a principal page of application</td>
               <td>25 minutes</td>
               <td>4h About</td>
-              <td>Done</td>
+              <td>
+                <StatusTask statusColor={'done'}>Done</StatusTask>
+              </td>
             </tr>
           </tbody>
         </table>
